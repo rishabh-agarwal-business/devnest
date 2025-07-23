76a065a61b90755741ce8b7a6a74d5cfa2e58bb6
Rishabh Agarwal <rishabh.developer1509@gmail.com>
2025-07-24 01:30:17 +0530

    Adding assets and metadata

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -11,6 +11,7 @@ const compat = new FlatCompat({
   allConfig: js.configs.all,
 });
 
+// eslint-disable-next-line import/no-anonymous-default-export
 export default [
   ...compat.extends(
     "next/core-web-vitals",
